94910ed3f8ced5663f1bab3454f29f06d9455e48
fujii noritsugu <quercus491@gmail.com>
2022-03-15 08:02:08 +0900

    プルリクエストのレビュー反映

--- test_python.py
+++ test_python.py
@@ -1,7 +1,7 @@
 def main():
     print('Hello Python PullRequest')
-    for i in range(3):
-        print(i)
+    for index, value in enumerate(range(3)):
+        print(f'index:{index} value:{value}')
 
 if __name__ == '__main__':
     main()
